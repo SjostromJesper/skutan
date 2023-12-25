aa5e2de294d2bd057dd2bafe102cc1ab10a6907c
Jesper Sjöström <j.sjostrom@live.se>
2023-12-25 17:44:48 +0100

    made it pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+              enabled: true
+          },
+          manifest: {
+              name: 'Skutan',
+              short_name: 'Skutan',
+              description: 'farfar martins skuta',
+              theme_color: '#ffffff',
+              icons: [
+                  {
+                      src: './assets/icons/boat.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: ['any', 'maskable']
+                  }
+              ]
+          }
+
+      })
+  ],
 })
